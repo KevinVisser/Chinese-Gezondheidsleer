93ba6cc6d9cd72673e310119cfcc217780122da8
Kevin <kevin.visser.1995@gmail.com>
2019-11-21 12:50:36 +0100

    deleted everything

--- preload.js
+++
@@ -1,8 +0,0 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-
-const remote = require('electron').remote;
-
-process.once('loaded', () => {
-  global.remote = remote;
-})
