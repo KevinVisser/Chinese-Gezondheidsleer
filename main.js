a2c79402c199a08aa24399af5718880d53e22915
Kevin <kevin.visser.1995@gmail.com>
2019-11-21 19:16:25 +0100

    intial commit

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 // Modules to control application life and create native browser window
 const {
-    app,
-    BrowserWindow
-  } = require('electron')
-  const path = require('path')
-  
-  const db = require('better-sqlite3')('./resources/app.db');
-  
-  const stmt = db.prepare('SELECT Nederlands FROM Kruiden');
-  console.log(stmt.all());
+  app,
+  BrowserWindow
+} = require('electron')
+const path = require('path')
+
+const db = require('better-sqlite3')('./resources/app.db');
+
+const stmt = db.prepare('SELECT Nederlands FROM Kruiden');
+console.log(stmt.all());
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     show: false,
@@ -28,17 +28,22 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // win.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
+
+  // Wait for 'ready-to-show' to display our windows
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  })
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
+    mainWindow = null
   })
 }
 
@@ -48,20 +53,16 @@ function createWindow () {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  if (process.platform !== 'darwin') app.quit()
 })
 
-app.on('activate', () => {
+app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
+  if (mainWindow === null) createWindow()
 })
 
 // In this file you can include the rest of your app's specific main process
