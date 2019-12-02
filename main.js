3231f247f8a7bc812c75f7d7bf3dc0e355353b2f
Kevin <kevin.visser.1995@gmail.com>
2019-12-02 09:36:30 +0100

    Database geïntegreerd, Wel global van gemaakt dus weet niet of dit de juiste manier is

--- main.js
+++ main.js
@@ -1,69 +1,68 @@
 // Modules to control application life and create native browser window
-const {
-  app,
-  BrowserWindow
-} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
 
-const db = require('better-sqlite3')('./resources/app.db');
+const db = require("better-sqlite3")("./resources/app.db");
 
-const stmt = db.prepare('SELECT Nederlands FROM Kruiden');
-console.log(stmt.all());
+global.database = db;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    show: false,
-    backgroundColor: '#FFF',
-    frame: false,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        show: false,
+        backgroundColor: "#FFF",
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+            preload: path.join(__dirname, "preload.js")
+        }
+    });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+    // and load the index.html of the app.
+    mainWindow.loadFile("index.html");
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
 
-  // Wait for 'ready-to-show' to display our windows
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-  })
+    //mainWindow.webContents.send("sendDB", db);
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    // Wait for 'ready-to-show' to display our windows
+    mainWindow.once("ready-to-show", () => {
+        mainWindow.show();
+    });
+
+    // Emitted when the window is closed.
+    mainWindow.on("closed", function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function() {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== "darwin") app.quit();
+});
 
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
-})
+app.on("activate", function() {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) createWindow();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -4,42 +4,66 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const remote = require("electron").remote;
+const ipcRenderer = require("electron").ipcRenderer;
+const db = remote.getGlobal("database");
 
+//const stmt = db.prepare("SELECT id, Nederlands FROM Kruiden");
 
 document.onreadystatechange = () => {
-    if(document.readyState == "complete") {
+    if (document.readyState == "complete") {
         HandleWindowControls();
     }
+};
+
+function getKruidenData() {
+    const kruidenData = db.prepare("SELECT * FROM Kruiden");
+    console.log(kruidenData.all());
+}
+
+function getPatentformuleData() {
+    const patentFormules = db.prepare("SELECT * FROM PatentFormules");
+    console.log(patentFormules.all());
+}
+
+function getSyndroomData() {
+    const syndroomData = db.prepare("SELECT * FROM Syndromen");
+    console.log(syndroomData.all());
+}
+
+function getSymptoomData() {
+    const symptoomData = db.prepare("SELECT * FROM Symptomen");
+    console.log(symptoomData.all());
 }
 
 function HandleWindowControls() {
     let win = remote.getCurrentWindow();
 
-    document.getElementById('min-button').addEventListener('click', event => {
+    document.getElementById("min-button").addEventListener("click", event => {
         win.minimize();
     });
 
-    document.getElementById('max-button').addEventListener('click', event => {
+    document.getElementById("max-button").addEventListener("click", event => {
         win.maximize();
     });
 
-    document.getElementById('restore-button').addEventListener('click', event => {
+    document.getElementById("restore-button").addEventListener("click", event => {
         win.unmaximize();
     });
 
-    document.getElementById('close-button').addEventListener('click', event => {
+    document.getElementById("close-button").addEventListener("click", event => {
         win.close();
     });
 
     ToggleMaxRestoreButtons();
-    win.on('maximize', ToggleMaxRestoreButtons);
-    win.on('unmaximize', ToggleMaxRestoreButtons);
+    win.on("maximize", ToggleMaxRestoreButtons);
+    win.on("unmaximize", ToggleMaxRestoreButtons);
 
     function ToggleMaxRestoreButtons() {
         if (win.isMaximized()) {
-            document.body.classList.add('maximized');
+            document.body.classList.add("maximized");
         } else {
-            document.body.classList.remove('maximized');
+            document.body.classList.remove("maximized");
         }
     }
 }
