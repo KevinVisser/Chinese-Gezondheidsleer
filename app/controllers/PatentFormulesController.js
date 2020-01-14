a7565d803b608b5ee68043efc59a05ded242ba82
Sander Beijaard <sanderbeijaard@hotmail.nl>
2020-01-14 12:15:27 +0100

    multple filters (wip)

--- app/controllers/PatentFormulesController.js
+++ app/controllers/PatentFormulesController.js
@@ -4,7 +4,6 @@ app.controller('PatentFormulesController', ['$routeParams', '$scope', '$location
     $scope.PatentFormulesModel = new PatentFormulesModel();
 
     $scope.patentFormules = $scope.PatentFormulesModel.GetRelevantData();
-    console.log("Hello, World");
 
     $scope.GoToView = function (id) {
         console.log(id);
@@ -15,3 +14,37 @@ app.controller('PatentFormulesController', ['$routeParams', '$scope', '$location
         $location.path('/Admin/Edit/PatentFormule/' + id);
     }
 }]);
+
+
+app.filter('split', function() {
+    return function(items, search) {
+        
+        if (!search) {
+            return items;
+        }
+
+        return items.filter(function(element, index, array) {
+            console.log("s: ", search);
+            console.log("e :", element);
+            
+            if (element.Nederlands != null && search.Nederlands != null && search.Nederlands != "") {
+                var tmp = element.Nederlands.toLowerCase();
+                var tmp2 = search.Nederlands.toLowerCase();
+
+                if(tmp != null) {
+                    var ned = tmp.includes(tmp2);
+                }
+            }
+            if (element.Engels != null && search.Engels != null && search.Engels != "") {
+                var tmp = element.Engels.toLowerCase();
+                var tmp2 = search.Engels.toLowerCase();
+
+                if(tmp != null) {                    
+                    var eng = tmp.includes(tmp2);
+                }
+            }
+            //hier moet de return
+            
+        });
+    }
+});
